22885db68d9b9f400174b785473c5d0f66e963ee
Mattias Olla <mattias.olla@gmail.com>
2021-08-03 16:33:46 +0200

    Add a CORS proxy
    This is an ugly solution, but for some reason a public API implements
    CORS, so we'll let a proxy query it for us.

--- src/parkingApi.js
+++ src/parkingApi.js
@@ -1,15 +1,16 @@
+const PROXY_URL = 'https://api.allorigins.win/get?url=';
 const API_URL = 'https://openparking.stockholm.se/LTF-Tolken/v1/ptillaten/street';
 const API_KEY = process.env.REACT_APP_PARKING_API_KEY;
 
 export async function getParkingInfo(streetName) {
+  const request = encodeURIComponent(
+    `${API_URL}/${streetName}?outputFormat=json&apiKey=${API_KEY}`
+  );
   try {
-    const result = await fetch(`${API_URL}/${streetName}?outputFormat=json&apiKey=${API_KEY}`, {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'application/json',
-      },
-    });
-    const data = await result.json();
+    const result = await fetch(PROXY_URL + request);
+    console.log(result);
+    const proxyData = await result.json();
+    const data = JSON.parse(proxyData.contents);
     return data.features;
   } catch (error) {
     console.error(error);
