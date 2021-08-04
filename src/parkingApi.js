19b78edb513e9a1407a46459f338d379d51b3908
Mattias Olla <mattias.olla@gmail.com>
2021-08-04 10:49:29 +0200

    WIP convert python code to JS

--- src/parkingApi.js
+++ src/parkingApi.js
@@ -27,3 +27,25 @@ export async function getSpacesByCoords(latitude, longitude, radius = 50) {
   );
   return await fetchJsonWithProxy(request);
 }
+
+export async function getServiceInfoByCoords(latitude, longitude, radius = 50) {
+  const request = encodeURIComponent(
+    `${API_URL}/servicedagar/within?radius=${radius}&lat=${latitude}&lng=${longitude}&outputFormat=json&apiKey=${API_KEY}`
+  );
+  return await fetchJsonWithProxy(request);
+}
+
+function isAllowed(spot, serviceInfo) {
+  const WEEKDAYS = ['söndag', 'måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag'];
+  const isRegulated = spot.VF_PLATS_TYP.contains('Tidsreglerad');
+  const isReserved = spot.VF_PLATS_TYP.contains('Reserverad');
+  if (Object.keys(serviceInfo).length) {
+    const now = new Date();
+    if (WEEKDAYS[now.getDay()] === serviceInfo.START_WEEKDAY) {
+      const currTimeWonkyFormat = 100 * now.getHours() + now;
+    }
+  }
+  if (isRegulated) return [false, 'Regulerad lastplats'];
+  if (isReserved) return [false, 'Ditt fordon får ej parkera här'];
+  return [true, ''];
+}
