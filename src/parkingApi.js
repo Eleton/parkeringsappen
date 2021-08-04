e6d6877536ac0cbe203aa285eb607e669609e440
Aiman <eleton@gmail.com>
2021-08-04 12:14:39 +0200

    WIP parkingApi

--- src/parkingApi.js
+++ src/parkingApi.js
@@ -35,14 +35,58 @@ export async function getServiceInfoByCoords(latitude, longitude, radius = 50) {
   return await fetchJsonWithProxy(request);
 }
 
+function before(time, comparison) {
+  if (time[0] < comparison[0]) {
+    return true;
+  } else {
+    return time[0] === comparison[0] && time[1] <= comparison[1];
+  }
+}
+
+function after(time, comparison) {
+  if (time[0] > comparison[0]) {
+    return true;
+  } else {
+    return time[0] === comparison[0] && time[1] >= comparison[1];
+  }
+}
+
+function distanceToOrigin(feature, lat, lng) {
+  const [fLng, fLat] = feature.geometry.coordinates[0];
+  return (lng - fLng) ** 2 + (lat - fLat) ** 2;
+}
+
+function cyclicBetween(time, start, end) {
+  if (before(end, start)) {
+    return before(time, end) || after(time, start);
+  } else {
+    return before(time, end) && after(time, start);
+  }
+}
+
 function isAllowed(spot, serviceInfo) {
   const WEEKDAYS = ['söndag', 'måndag', 'tisdag', 'onsdag', 'torsdag', 'fredag', 'lördag'];
   const isRegulated = spot.VF_PLATS_TYP.contains('Tidsreglerad');
   const isReserved = spot.VF_PLATS_TYP.contains('Reserverad');
   if (Object.keys(serviceInfo).length) {
+    const { START_WEEKDAY, START_TIME, END_TIME, START_MONTH, END_MONTH, START_DAY, END_DAY } =
+      serviceInfo;
     const now = new Date();
-    if (WEEKDAYS[now.getDay()] === serviceInfo.START_WEEKDAY) {
-      const currTimeWonkyFormat = 100 * now.getHours() + now;
+    if (WEEKDAYS[now.getDay()] === START_WEEKDAY) {
+      const startTime = [Math.floor(START_TIME / 100), START_TIME % 100];
+      const endTime = [Math.floor(END_TIME / 100), END_TIME % 100];
+      if (cyclicBetween([now.getHours(), now.getMinutes()], startTime, endTime)) {
+        if (!START_MONTH) return [false, 'Servicetid'];
+        // getMonth indexes from 0...
+        if (
+          cyclicBetween(
+            [now.getMonth() + 1, now.getDate()],
+            [START_MONTH, START_DAY],
+            [END_MONTH, END_DAY]
+          )
+        )
+          return [false, 'Servicetid'];
+      }
     }
   }
   if (isRegulated) return [false, 'Regulerad lastplats'];
